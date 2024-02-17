47c598f4a6d5707f5efbcd8849b0b0fa76e53d02
RockyPHER <davidsonjunior10@gmail.com>
2024-02-17 01:40:04 -0300

    Complete: First Static Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
-"use client";
 import musashiImage from "@/images/musashi.png";
 import Image from "next/image";
-import { NextUIProvider } from "@nextui-org/react";
+import NextJS from "@/icons/Nextjs.svg";
+import Python from "@/icons/python.svg";
+import Rust from "@/icons/Rust.svg";
 
 export default function Home() {
   return (
-    <NextUIProvider>
-      <main>
-        <div className="w-[90%] h-[90%] bg-gray-400">
-          <Image
-            className="absolute -right-40 top-0 w-[790px] h-[630px]"
-            src={musashiImage}
-            alt="musashi"
-            width={1300}
-            height={800}
-          />
+    <div className="w-full h-auto flex items-center justify-center overflow-hidden">
+      {/* first screen */}
+      <div className="w-full h-[642px] relative flex justify-center items-center">
+        <div className="w-[1280px] h-[580px] flex flex-col justify-between py-8 px-10 border-2 border-black">
+          <span>
+            <h1>Davidson Amaro</h1>
+            <h2>Software Developer</h2>
+            <h2>Designer UX/UI</h2>
+            <h2>Video Editor</h2>
+          </span>
+          <ul className="flex gap-4">
+            <li>
+              <NextJS className="w-16 h-16" />
+            </li>
+            <li>
+              <Python className="w-16 h-16" />
+            </li>
+            <li>
+              <Rust className="w-16 h-16" />
+            </li>
+          </ul>
         </div>
-      </main>
-    </NextUIProvider>
+        <Image
+          className="w-[710px] absolute top-0 -right-[150px]"
+          src={musashiImage}
+          alt="musashi"
+          width={1300}
+          height={800}
+        />
+      </div>
+    </div>
   );
 }
